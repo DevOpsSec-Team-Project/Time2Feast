7cdf1d6e14cb1fddb870d69d0497a513fb894b82
michaele490 <michaelegharevba490@gmail.com>
2025-04-02 14:55:32 +0100

    Deployment job adjustment ~

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
+# Deployment script
+
 # Load bashrc
 source ~/.bashrc
+
 # Properly load rbenv
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-# Go to project folder
-cd Time2Feast
+
 # Installing dependencies
 bundle install
+
 # Set environment to production
 # export RAILS_ENV = production
+
 # Run migrations on database
 # rails db:migrate
+
 # Run server (Port 80 for now until production ready)
 sudo -E /home/ubuntu/.rbenv/shims/bundle exec rails server --binding 0.0.0.0 --port 80
