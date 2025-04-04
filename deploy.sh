44108a86b5e181075ae2537f4678f57b863834fd
michaele490 <michaelegharevba490@gmail.com>
2025-04-04 11:55:09 +0100

    Adding migration command to deployment script and removing key step to deploy job

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ eval "$(rbenv init -)"
 # Installing dependencies
 bundle install
 
+# Run migrations
+rails db:migrate
+
 # Set environment to production
 # export RAILS_ENV = production
 
@@ -18,5 +21,3 @@ bundle install
 
 # Run server (Port 80 for now until production ready)
 sudo -E /home/ubuntu/.rbenv/shims/bundle exec rails server --binding 0.0.0.0 --port 80
-
-# Deployment push test now that instance is running
