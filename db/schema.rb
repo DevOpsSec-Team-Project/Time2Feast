cf23dd5fde109b3fafd4b9c05ada5908200f2606
michaele490 <michaelegharevba490@gmail.com>
2025-04-05 05:48:01 +0100

    Generating Recipe scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_03_180237) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_05_044545) do
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "duration"
+    t.integer "minutes"
+    t.integer "hours"
+    t.text "ingredients"
+    t.text "steps"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
