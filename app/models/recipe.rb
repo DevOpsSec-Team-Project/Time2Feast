b212ca5e94a34a87fa5bdc498cd8cad69c61a8f8
michaele490 <michaelegharevba490@gmail.com>
2025-04-05 05:59:40 +0100

    Adding logic to User and Recipe models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,27 @@
 class Recipe < ApplicationRecord
+
+  belongs_to :User
+
+  #Store as YAML in database, but show as array
+
+  # Ingredients getter
+  def Ingredients
+    YAML.load(read_attribute(:ingredients)) rescue[]
+  end
+
+  # Ingredients setter
+  def ingredients=(value)
+    write_attribute(:ingredients, value.to_yaml)
+  end
+
+  # Steps getter
+  def Steps
+    YAML.load(read_attribute(:steps)) rescue []
+  end
+
+  # Steps setter
+  def steps=(value)
+    write_attribute(:steps, value.to_yaml)
+  end
+  
 end
